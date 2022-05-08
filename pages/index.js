5a32cddee852b196c4fca064fe61e0618c9caa18
Tony <vutoan266@gmail.com>
2022-05-08 11:01:13 +0700

    The first commit

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import MenuPage from "../src/containers/Menu";
+
+export default function Home() {
+  return <MenuPage />;
+}
---
+++ src/apis/index.js
@@ -0,0 +1,3 @@
+import request from "../utils/request";
+
+export const getMenuAPI = () => request.get("");
